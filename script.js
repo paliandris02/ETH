0954fa6fb8ea896e6a42746f6e899f082f2deed6
paliandris02 <pali.andris@gmail.com>
2022-09-02 01:30:03 +0200

    jsFinal

--- script.js
+++ script.js
@@ -1,71 +1,69 @@
 "use strict";
 
 const wallets = document.getElementById("selected_wallet").options;
+const receiveAmount = document.getElementById("receive");
+const feeAmount = document.getElementById("fee");
 
-const state = {
-  fastTransfer: 0.0001913,
-  standardTransfer: 0.00005631,
+const transferType = new Map();
+transferType.set(false, 0.00005631).set(true, 0.0001913);
+
+const walletsOptions = {
+  wallets: [
+    {
+      name: "Argent",
+      fee: 0.001,
+    },
+    {
+      name: "Dharma",
+      fee: 0.005,
+    },
+    {
+      name: "Coinbase",
+      fee: 0.014,
+    },
+    {
+      name: "AlphaWallet",
+      fee: 0.008,
+    },
+  ],
 };
 
 const calcBtn = document.querySelector(".calc");
+
 calcBtn.addEventListener("click", (e) => {
   const selectedWallet =
     document.getElementById("selected_wallet").selectedIndex;
-  console.log(wallets[selectedWallet]);
+  const walletName = wallets[selectedWallet].value;
+
+  const amount = document.getElementById("send");
+
+  if (amount.value <= 0 || isNaN(amount.value)) {
+    amount.value = "Invalid.";
+    return;
+  }
+  render(walletName, amount.value);
 });
-function calculate() {
-  let fast = 0.0001913;
-  let standard = 0.00005631;
-  if (document.getElementById("send").value <= 0) {
-    document.getElementById("send").value = "INVALID";
-  } else {
-    if (b[a].value == "Argent") {
-      if (document.getElementById("under2m").checked) {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value = amount * Number(0.98) - fast;
-        document.getElementById("fee").value = amount * Number(0.02) + fast;
-      } else {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value =
-          amount * Number(0.98) - standard;
-        document.getElementById("fee").value = amount * Number(0.02) + standard;
-      }
-    }
-    if (b[a].value == "Dharma") {
-      if (document.getElementById("under2m").checked) {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value = amount * Number(0.98) - fast;
-        document.getElementById("fee").value = amount * Number(0.02) + fast;
-      } else {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value =
-          amount * Number(0.98) - standard;
-        document.getElementById("fee").value = amount * Number(0.02) + standard;
-      }
-    }
-    if (b[a].value == "Coinbase") {
-      if (document.getElementById("under2m").checked) {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value = amount * Number(0.98) - fast;
-        document.getElementById("fee").value = amount * Number(0.02) + fast;
-      } else {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value =
-          amount * Number(0.98) - standard;
-        document.getElementById("fee").value = amount * Number(0.02) + standard;
-      }
-    }
-    if (b[a].value == "AlphaWallet") {
-      if (document.getElementById("under2m").checked) {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value = amount * Number(0.98) - fast;
-        document.getElementById("fee").value = amount * Number(0.02) + fast;
-      } else {
-        let amount = document.getElementById("send").value;
-        document.getElementById("receive").value =
-          amount * Number(0.98) - standard;
-        document.getElementById("fee").value = amount * Number(0.02) + standard;
-      }
+
+const render = function (selectedWalletName, amount) {
+  const isFastTransfer = document.getElementById("under2m").checked;
+  let fees;
+  for (const { name, fee } of walletsOptions.wallets) {
+    if (name === selectedWalletName) {
+      fees = fee;
+      break;
     }
   }
-}
+  renderAmounts(isFastTransfer, amount, fees);
+};
+
+const renderAmounts = function (isFastTransfer, amount, fees) {
+  receiveAmount.value = (
+    Number(amount) -
+    Number(amount) * Number(fees) -
+    transferType.get(isFastTransfer)
+  ).toFixed(5);
+  feeAmount.textContent = `Total fee: ${(
+    Number(amount) * Number(fees) +
+    transferType.get(isFastTransfer)
+  ).toFixed(5)}`;
+};
